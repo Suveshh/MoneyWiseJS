1dc7688da78b60c3a9afa574ee439968621ea4ff
Saswat <saswatchoudhury8@gmail.com>
2025-07-06 11:12:41 +0530

    auth update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+
 const rootElement = document.getElementById("root");
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+     <App/>
     </StrictMode>
   );
 }
